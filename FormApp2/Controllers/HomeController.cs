7ddb44ac6eb6a1b61c79f8ae7a9d1db9ee1388fc
Muhammed Yasin Özdemir <94251353+MuhammedYasinOzdemirDev@users.noreply.github.com>
2024-02-13 22:05:32 +0300

    Add files via upload

---
+++ FormApp2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FormApp2.Models;
+
+namespace FormApp2.Controllers;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    public IActionResult Search(string q)
+    {
+        string a = "";
+        if (q != null)
+            a = q;
+        ViewBag.aa = a;
+        return View();
+    }
+
+
+}
